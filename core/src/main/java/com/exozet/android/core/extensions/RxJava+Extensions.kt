d0cbcb48fbfd4bd1abe0b4d32412efc1ce839924
A Shkurti <shkurtiarmando@gmail.com>
2018-01-31 15:12:25 +0100

    Add kotlin extension for rxjava subscribe

---
+++ core/src/main/java/com/exozet/android/core/extensions/RxJava+Extensions.kt
@@ -0,0 +1,33 @@
+package com.exozet.android.core.extensions
+
+import io.reactivex.Flowable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+
+/**
+ * Created by armando.shkurti on 31.01.18.
+ */
+fun <T> Flowable<T>.applySchedulersIO(): Flowable<T> {
+    return subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+}
+
+fun <T> Flowable<T>.applySchedulersComputation(): Flowable<T> {
+    return subscribeOn(Schedulers.computation()).observeOn(AndroidSchedulers.mainThread())
+}
+
+fun <T> Flowable<T>.subscribeUIToIO(onNext: (T) -> Unit, onError: (Throwable) -> Unit): Disposable {
+    return applySchedulersIO().subscribe({ next ->
+        onNext(next)
+    }, { error ->
+        onError(error)
+    })
+}
+
+fun <T> Flowable<T>.subscribeUIToComputation(onNext: (T) -> Unit, onError: (Throwable) -> Unit): Disposable {
+    return applySchedulersComputation().subscribe({ next ->
+        onNext(next)
+    }, { error ->
+        onError(error)
+    })
+}
