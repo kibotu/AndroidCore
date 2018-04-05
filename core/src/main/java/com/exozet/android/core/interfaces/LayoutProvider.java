59d5728e0c4093715fc2d3c1a98a0cfd0aacb371
Habib Birdal <habib.birdal@exozet.com>
2018-04-05 12:43:52 +0200

    added the interface provider and FragmentExtensions

---
+++ core/src/main/java/com/exozet/android/core/interfaces/LayoutProvider.java
@@ -0,0 +1,12 @@
+package com.exozet.android.core.interfaces;
+
+import android.support.annotation.LayoutRes;
+
+/**
+ * Created by jan.rabe on 02/02/16.
+ */
+public interface LayoutProvider {
+
+    @LayoutRes
+    int getLayout();
+}
