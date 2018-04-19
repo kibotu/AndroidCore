673418ff8ad416e8a1449186892020b41175e23f
Jan Rabe <jan.rabe@exozet.com>
2018-04-19 17:57:09 +0200

    updated audio manager extensions

--- core/src/main/java/com/exozet/android/core/extensions/AudioManager+Extensions.kt
+++ core/src/main/java/com/exozet/android/core/extensions/AudioManager+Extensions.kt
@@ -3,54 +3,47 @@
 package com.exozet.android.core.extensions
 
 import android.media.AudioManager
-import android.os.Build
+import android.media.AudioManager.*
+import android.os.Build.VERSION.SDK_INT
+import android.os.Build.VERSION_CODES.M
 import android.support.v4.math.MathUtils.clamp
 
 /**
  * Created by [Jan Rabe](https://about.me/janrabe).
  */
 
-private var _isMuted = false
+private var _isMuted: Boolean = false
 
-val AudioManager.isMuted: Boolean
+var AudioManager.isMuted: Boolean
     get() = _isMuted
+    set(value) {
+        _isMuted = value
+    }
 
 @Suppress("DEPRECATION")
-fun AudioManager.muteAll() {
-    _isMuted = true
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        adjustStreamVolume(AudioManager.STREAM_NOTIFICATION, AudioManager.ADJUST_MUTE, 0)
-        adjustStreamVolume(AudioManager.STREAM_ALARM, AudioManager.ADJUST_MUTE, 0)
-        adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_MUTE, 0)
-        adjustStreamVolume(AudioManager.STREAM_RING, AudioManager.ADJUST_MUTE, 0)
-        adjustStreamVolume(AudioManager.STREAM_SYSTEM, AudioManager.ADJUST_MUTE, 0)
-    } else {
-        setStreamMute(AudioManager.STREAM_NOTIFICATION, true)
-        setStreamMute(AudioManager.STREAM_ALARM, true)
-        setStreamMute(AudioManager.STREAM_MUSIC, true)
-        setStreamMute(AudioManager.STREAM_RING, true)
-        setStreamMute(AudioManager.STREAM_SYSTEM, true)
+fun AudioManager.mute(mute: Boolean = true) {
+    isMuted = mute
+    if (SDK_INT >= M) {
+        val flag = if (mute) ADJUST_MUTE else ADJUST_UNMUTE
+        adjustStreamVolume(STREAM_NOTIFICATION, flag, 0)
+        adjustStreamVolume(STREAM_ALARM, flag, 0)
+        adjustStreamVolume(STREAM_MUSIC, flag, 0)
+        adjustStreamVolume(STREAM_RING, flag, 0)
+        adjustStreamVolume(STREAM_SYSTEM, flag, 0)
     }
+//    else {
+    setStreamMute(STREAM_NOTIFICATION, mute)
+    setStreamMute(STREAM_ALARM, mute)
+    setStreamMute(STREAM_MUSIC, mute)
+    setStreamMute(STREAM_RING, mute)
+    setStreamMute(STREAM_SYSTEM, mute)
+//    }
 }
 
 @Suppress("DEPRECATION")
-fun AudioManager.unmuteAll() {
-    _isMuted = false
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        adjustStreamVolume(AudioManager.STREAM_NOTIFICATION, AudioManager.ADJUST_UNMUTE, 0)
-        adjustStreamVolume(AudioManager.STREAM_ALARM, AudioManager.ADJUST_UNMUTE, 0)
-        adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_UNMUTE, 0)
-        adjustStreamVolume(AudioManager.STREAM_RING, AudioManager.ADJUST_UNMUTE, 0)
-        adjustStreamVolume(AudioManager.STREAM_SYSTEM, AudioManager.ADJUST_UNMUTE, 0)
-    } else {
-        setStreamMute(AudioManager.STREAM_NOTIFICATION, false)
-        setStreamMute(AudioManager.STREAM_ALARM, false)
-        setStreamMute(AudioManager.STREAM_MUSIC, false)
-        setStreamMute(AudioManager.STREAM_RING, false)
-        setStreamMute(AudioManager.STREAM_SYSTEM, false)
-    }
+fun AudioManager.unmute() {
+    mute(false)
 }
-
 
 fun AudioManager.decreaseVolume() {
     changeVolume(-10)
@@ -61,14 +54,11 @@ fun AudioManager.increaseVolume() {
 }
 
 fun AudioManager.toggleMute() {
-    if (isMuted)
-        unmuteAll()
-    else
-        muteAll()
+    mute(!isMuted)
 }
 
 fun AudioManager.changeVolume(dt: Int) {
-    val streamMaxVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
+    val streamMaxVolume = audioManager.getStreamVolume(STREAM_MUSIC)
     val volume = clamp(streamMaxVolume + dt, 0, streamMaxVolume)
-    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volume, 0)
+    audioManager.setStreamVolume(STREAM_MUSIC, volume, 0)
 }
