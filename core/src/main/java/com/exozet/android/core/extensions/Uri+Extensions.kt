2e6d68b5ce65c9a38f0210ffc9d251ce768325f0
Jan Rabe <jan.rabe@exozet.com>
2018-10-17 13:29:46 +0200

    updated uri extensions

--- core/src/main/java/com/exozet/android/core/extensions/Uri+Extensions.kt
+++ core/src/main/java/com/exozet/android/core/extensions/Uri+Extensions.kt
@@ -1,18 +1,21 @@
 package com.exozet.android.core.extensions
 
-import android.content.Context
 import android.net.Uri
 import android.os.Environment
+import net.kibotu.ContextHelper
 import java.io.File
 
 /**
  * Created by [Jan Rabe](https://about.me/janrabe).
  */
 
-fun parseAssetFile(file: String): Uri = Uri.parse("file:///android_asset/$file")
+fun String.parseAssetFile(): Uri = Uri.parse("file:///android_asset/$this")
 
-fun parseInternalStorageFile(context: Context, file: String): Uri = Uri.parse("${context.filesDir.absolutePath}/$file")
+fun String.parseInternalStorageFile(): Uri = Uri.parse("${ContextHelper.getApplication()!!.filesDir.absolutePath}/$this")
 
-fun parseExternalStorageFile(file: String): Uri = Uri.parse("${Environment.getExternalStorageDirectory()}/$file")
+fun String.parseExternalStorageFile(): Uri = Uri.parse("${Environment.getExternalStorageDirectory()}/$this")
 
-fun parseFile(file: String): Uri = Uri.fromFile(File(file))
+fun String.parseFile(): Uri = Uri.fromFile(File(this))
+
+val Uri.fileExists: Boolean
+    get() = File(toString()).exists()
