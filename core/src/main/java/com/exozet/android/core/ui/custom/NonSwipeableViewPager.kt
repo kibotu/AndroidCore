83279ac7701e2c43e63dbdd5752930509252e51c
Jan Rabe <jan.rabe@exozet.com>
2019-01-29 13:24:38 +0100

    added non swipeable viewpager

---
+++ core/src/main/java/com/exozet/android/core/ui/custom/NonSwipeableViewPager.kt
@@ -0,0 +1,13 @@
+package com.exozet.android.core.ui.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.viewpager.widget.ViewPager
+
+class NonSwipeableViewPager constructor(context: Context, attrs: AttributeSet?) : ViewPager(context, attrs) {
+
+    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean = false
+
+    override fun onTouchEvent(ev: MotionEvent?): Boolean = false
+}
