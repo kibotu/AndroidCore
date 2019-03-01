68e54a5b7139799f30b8e953207dd9bb6e0aaff0
Jan Rabe <jan.rabe@exozet.com>
2019-03-01 10:48:42 +0100

    added shatest

---
+++ app/src/test/java/com/exozet/android/core/sha256Test.kt
@@ -0,0 +1,30 @@
+package com.exozet.android.core
+
+import com.exozet.android.core.base.BaseTest
+import com.google.common.truth.Truth
+import org.junit.Test
+
+
+class sha : BaseTest() {
+    /**
+     * https://tools.ietf.org/html/rfc7636#appendix-A
+     */
+    @Test
+    fun sha256_HMACTest() {
+        Truth.assertThat("any carnal pleasure.".sha256_HMAC()).isEqualTo("klV7mnDnMorgaiIdbLmVSjsQDWP3UpClQSSnaI2nOXE=")
+    }
+
+    @Test
+    fun retrievalTokenEncryptionTest() {
+        val actual = "any carnal pleasure.".sha256_HMAC()
+
+        val expected = "klV7mnDnMorgaiIdbLmVSjsQDWP3UpClQSSnaI2nOXE="
+        Truth.assertThat(actual).isEqualTo(expected)
+
+        val expected2 = "klV7mnDnMorgaiIdbLmVSjsQDWP3UpClQSSnaI2nOXE"
+        val removedCharacters = actual.replace("+", "-")
+            .replace("/", "_")
+            .replace("=", "")
+        Truth.assertThat(removedCharacters).isEqualTo(expected2)
+    }
+}
