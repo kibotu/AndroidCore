61661f6df5d960f7744e359024409aca46fddeef
Jan Rabe <jan.rabe@exozet.com>
2019-03-28 11:04:22 +0100

    fixed compile error

--- core/src/main/java/com/exozet/android/core/extensions/RecyclerView+Extensions.kt
+++ core/src/main/java/com/exozet/android/core/extensions/RecyclerView+Extensions.kt
@@ -3,7 +3,6 @@
 package com.exozet.android.core.extensions
 
 import androidx.recyclerview.widget.RecyclerView
-import net.kibotu.android.recyclerviewpresenter.PresenterAdapter
 
 /**
  * Created by [Jan Rabe](https://about.me/janrabe).
@@ -11,12 +10,4 @@ import net.kibotu.android.recyclerviewpresenter.PresenterAdapter
 
 fun <VH> RecyclerView.Adapter<VH>.isEmpty(): Boolean where VH : RecyclerView.ViewHolder {
     return itemCount == 0
-}
-
-fun <T> PresenterAdapter<T>.forEach(block: (T) -> Unit) {
-    (0 until itemCount).forEach { block(get(it)) }
-}
-
-fun <T> PresenterAdapter<T>.forEachIndexed(block: (Int, T) -> Unit) {
-    (0 until itemCount).forEachIndexed { index, t -> block(index, get(t)) }
 }
