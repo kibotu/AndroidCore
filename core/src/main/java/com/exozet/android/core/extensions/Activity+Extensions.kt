eccdf81723a1e6289db0470f886bed28cc66300b
Jan Rabe <jan.rabe@exozet.com>
2019-04-11 18:07:58 +0200

    added choose wifi start activity extension

--- core/src/main/java/com/exozet/android/core/extensions/Activity+Extensions.kt
+++ core/src/main/java/com/exozet/android/core/extensions/Activity+Extensions.kt
@@ -3,9 +3,13 @@
 package com.exozet.android.core.extensions
 
 import android.app.Activity
+import android.content.Intent
+import android.net.wifi.WifiManager
 import android.view.ViewGroup
 
 val Activity.contentRootView: ViewGroup
     get() = window
         .decorView
         .findViewById(android.R.id.content)
+
+fun Activity.chooseWifiNetwork() = startActivity(Intent(WifiManager.ACTION_PICK_WIFI_NETWORK))
