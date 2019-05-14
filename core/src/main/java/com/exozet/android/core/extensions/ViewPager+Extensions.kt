fab6f1b08bae5b5cf952d40b9ef82ef67dec2621
Jan Rabe <jan.rabe@exozet.com>
2019-05-14 09:18:11 +0200

    removed deprecated firebase instance id class

--- core/src/main/java/com/exozet/android/core/extensions/ViewPager+Extensions.kt
+++ core/src/main/java/com/exozet/android/core/extensions/ViewPager+Extensions.kt
@@ -1,2 +1,14 @@
+@file:JvmName("ViewPagerExtensions")
+
+
 package com.exozet.android.core.extensions
 
+import androidx.viewpager.widget.ViewPager
+
+fun ViewPager.scrollRight(pages: Int = 1) {
+    setCurrentItem(currentItem.plus(pages).clamp(0, (adapter?.count ?: 0) - 1), true)
+}
+
+fun ViewPager.scrollLeft(pages: Int = 1) {
+    setCurrentItem(currentItem.minus(pages).clamp(0, (adapter?.count ?: 0) - 1), true)
+}
