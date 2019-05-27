b0247f9dc1bd75388153cad49b865b57f7f7bcc7
Jan Rabe <jan.rabe@exozet.com>
2019-05-27 20:50:58 +0200

    fixed koin inject extension

--- core/src/main/java/com/exozet/android/core/extensions/Koin+Extensions.kt
+++ core/src/main/java/com/exozet/android/core/extensions/Koin+Extensions.kt
@@ -5,17 +5,15 @@ package com.exozet.android.core.extensions
 import org.koin.core.context.GlobalContext
 import org.koin.core.parameter.ParametersDefinition
 import org.koin.core.qualifier.Qualifier
-import org.koin.core.scope.Scope
 
 
 /**
- * inject lazily given dependency for Any
+ * inject lazily given dependency for Android koincomponent
  * @param qualifier - bean qualifier / optional
  * @param scope
  * @param parameters - injection parameters
  */
 inline fun <reified T : Any> Any.inject(
     qualifier: Qualifier? = null,
-    scope: Scope? = null,
     noinline parameters: ParametersDefinition? = null
-) = lazy { GlobalContext.get().koin.get<T>(qualifier, scope, parameters) }
+) = lazy { GlobalContext.get().koin.get<T>(qualifier, parameters) }
