f54f751286b43a5bfe860a7bf2a26f2c8f318286
Jan Rabe <jan.rabe@exozet.com>
2019-06-13 19:02:04 +0200

    updated realm live data

--- core/src/main/java/com/exozet/android/core/extensions/Realm+Extensions.kt
+++ core/src/main/java/com/exozet/android/core/extensions/Realm+Extensions.kt
@@ -1,11 +1,36 @@
 package com.exozet.android.core.extensions
 
-import com.exozet.android.core.realm.RealmLiveData
+import androidx.lifecycle.LiveData
+import io.realm.RealmChangeListener
 import io.realm.RealmModel
 import io.realm.RealmResults
+import io.realm.kotlin.addChangeListener
+import io.realm.kotlin.removeChangeListener
 
-/**
- * Created by Tim Wienrich
- */
+open class RealmResultsLiveData<T : RealmModel>(val results: RealmResults<T>) : LiveData<RealmResults<T>>() {
 
-inline fun <reified T : RealmModel> RealmResults<T>.asLiveData() = RealmLiveData(this)
+    protected val listener = RealmChangeListener<RealmResults<T>> { value = it }
+
+    override fun onActive() {
+        results.addChangeListener(listener)
+        value = results
+    }
+
+    override fun onInactive() = results.removeChangeListener(listener)
+}
+
+open class RealmModelLiveData<T : RealmModel>(val model: T) : LiveData<T>() {
+
+    protected val listener = RealmChangeListener<T> { value = it }
+
+    override fun onActive() {
+        model.addChangeListener(listener)
+        value = model
+    }
+
+    override fun onInactive() = model.removeChangeListener(listener)
+}
+
+inline fun <reified T : RealmModel> RealmResults<T>.asLiveData() = RealmResultsLiveData(this)
+
+inline fun <reified T : RealmModel> T.asLiveData() = RealmModelLiveData(this)
