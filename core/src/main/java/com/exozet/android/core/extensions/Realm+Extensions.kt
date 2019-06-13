7c7a49d0802113e9f068299bc0c9f0c50f656828
Jan Rabe <jan.rabe@exozet.com>
2019-06-13 19:16:06 +0200

    added condition for active value

--- core/src/main/java/com/exozet/android/core/extensions/Realm+Extensions.kt
+++ core/src/main/java/com/exozet/android/core/extensions/Realm+Extensions.kt
@@ -7,30 +7,32 @@ import io.realm.RealmResults
 import io.realm.kotlin.addChangeListener
 import io.realm.kotlin.removeChangeListener
 
-open class RealmResultsLiveData<T : RealmModel>(val results: RealmResults<T>) : LiveData<RealmResults<T>>() {
+open class RealmResultsLiveData<T : RealmModel>(val results: RealmResults<T>, var valueOnActive: Boolean = false) : LiveData<RealmResults<T>>() {
 
     protected val listener = RealmChangeListener<RealmResults<T>> { value = it }
 
     override fun onActive() {
         results.addChangeListener(listener)
-        value = results
+        if (valueOnActive)
+            value = results
     }
 
     override fun onInactive() = results.removeChangeListener(listener)
 }
 
-open class RealmModelLiveData<T : RealmModel>(val model: T) : LiveData<T>() {
+open class RealmModelLiveData<T : RealmModel>(val model: T, var valueOnActive: Boolean = false) : LiveData<T>() {
 
     protected val listener = RealmChangeListener<T> { value = it }
 
     override fun onActive() {
         model.addChangeListener(listener)
-        value = model
+        if (valueOnActive)
+            value = model
     }
 
     override fun onInactive() = model.removeChangeListener(listener)
 }
 
-inline fun <reified T : RealmModel> RealmResults<T>.asLiveData() = RealmResultsLiveData(this)
+inline fun <reified T : RealmModel> RealmResults<T>.asLiveData(valueOnActive: Boolean = false) = RealmResultsLiveData(this, valueOnActive)
 
-inline fun <reified T : RealmModel> T.asLiveData() = RealmModelLiveData(this)
+inline fun <reified T : RealmModel> T.asLiveData(valueOnActive: Boolean = false) = RealmModelLiveData(this, valueOnActive)
