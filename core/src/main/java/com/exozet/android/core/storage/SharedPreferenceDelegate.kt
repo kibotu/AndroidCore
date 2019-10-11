3a1e8b7900b5c0052dc3c52c3892e893280e3b7d
Jan Rabe <jan.rabe@exozet.com>
2019-10-11 11:35:23 +0200

    updated shared preference delegate to support parceables

--- core/src/main/java/com/exozet/android/core/storage/SharedPreferenceDelegate.kt
+++ core/src/main/java/com/exozet/android/core/storage/SharedPreferenceDelegate.kt
@@ -3,10 +3,13 @@ package com.exozet.android.core.storage
 import android.content.Context
 import android.content.SharedPreferences
 import android.content.SharedPreferences.Editor
+import android.os.Parcelable
 import android.preference.PreferenceManager
 import com.exozet.android.core.extensions.lazyFast
 import com.exozet.android.core.extensions.safeContext
-import net.kibotu.ContextHelper
+import com.exozet.android.core.gson.fromJson
+import com.exozet.android.core.gson.toJson
+import com.github.florent37.application.provider.application
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
@@ -39,10 +42,16 @@ private class SharedPreferenceDelegate<T>(
 @Suppress("UNCHECKED_CAST")
 fun <T> sharedPreference(key: String, defaultValue: T): ReadWriteProperty<Any, T> =
     when (defaultValue) {
-        is Boolean -> SharedPreferenceDelegate(ContextHelper.getApplication() as Context, defaultValue, SharedPreferences::getBoolean, Editor::putBoolean, key)
-        is Int -> SharedPreferenceDelegate(ContextHelper.getApplication() as Context, defaultValue, SharedPreferences::getInt, Editor::putInt, key)
-        is Long -> SharedPreferenceDelegate(ContextHelper.getApplication() as Context, defaultValue, SharedPreferences::getLong, Editor::putLong, key)
-        is Float -> SharedPreferenceDelegate(ContextHelper.getApplication() as Context, defaultValue, SharedPreferences::getFloat, Editor::putFloat, key)
-        is String -> SharedPreferenceDelegate(ContextHelper.getApplication() as Context, defaultValue, SharedPreferences::getString, Editor::putString, key)
+        is Parcelable -> SharedPreferenceDelegate(application!!, defaultValue, SharedPreferences::getParcelable, Editor::putParcelable, key)
+        is Boolean -> SharedPreferenceDelegate(application!!, defaultValue, SharedPreferences::getBoolean, Editor::putBoolean, key)
+        is Int -> SharedPreferenceDelegate(application!!, defaultValue, SharedPreferences::getInt, Editor::putInt, key)
+        is Long -> SharedPreferenceDelegate(application!!, defaultValue, SharedPreferences::getLong, Editor::putLong, key)
+        is Float -> SharedPreferenceDelegate(application!!, defaultValue, SharedPreferences::getFloat, Editor::putFloat, key)
+        is String -> SharedPreferenceDelegate(application!!, defaultValue, SharedPreferences::getString, Editor::putString, key)
         else -> throw IllegalArgumentException()
     } as ReadWriteProperty<Any, T>
+
+
+inline fun <reified T : Parcelable> SharedPreferences.getParcelable(key: String, defValue: T): T = getString(key, defValue.toJson())!!.fromJson()
+
+inline fun <reified T : Parcelable> Editor.putParcelable(key: String, value: T): Editor = putString(key, value.toJson())
